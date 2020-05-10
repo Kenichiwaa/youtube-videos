0fd1629c0cd6dd7ce466b9e2c2486ae62931cd37
Ken Sugai <kenichisugai@gmail.com>
2020-05-10 16:29:41 -0700

    build axios call to youtube

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -2,12 +2,7 @@ import axios from 'axios';
 
 const KEY = 'AIzaSyAM7PUONBJYrgDIEvCIAYmnUNMrnnD-7bI';
 
-
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY
-    }
+
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+
+const KEY = 'AIzaSyAM7PUONBJYrgDIEvCIAYmnUNMrnnD-7bI';
 
 class App extends React.Component {
+
+  state = { videos: []};
+
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        q: term,
+        key: KEY
+      }
+    });
+    this.setState({videos: response.data.items})
+  }
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit}/>
+        I have {this.state.videos.length} videos
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
